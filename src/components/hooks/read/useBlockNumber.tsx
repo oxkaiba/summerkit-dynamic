b7afe134a81229077ee7ff6829aa94249d100cc4
kaiba <96191847+oxkaiba@users.noreply.github.com>
2024-07-09 18:26:21 +0100

    Update useBlockNumber.tsx

--- src/components/hooks/read/useBlockNumber.tsx
+++ src/components/hooks/read/useBlockNumber.tsx
@@ -46,7 +46,7 @@ export default function BlockNumber() {
                   <span className="bg-blue-500 rounded-full ml-1.5 h-2 w-2 inline-block animate-pulse"></span>
                 </div>
               </HoverCardTrigger>
-              <HoverCardContent className='bg-accent border-zinc-800/50 text-xs font-medium ml-2 mb-4 rounded-full px-4'>
+             <HoverCardContent className='bg-card border-zinc-800/50 text-xs font-medium ml-2 mb-4 rounded-full'>
                 The most recent block on Base.
               </HoverCardContent>
             </HoverCard>
